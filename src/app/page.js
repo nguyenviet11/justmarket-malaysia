6d4db487f18383b55f86e85c439fe7bea220c9ab
vietnv <nguyenvanvietbn1998@gmail.com>
2024-10-02 21:02:26 +0700

    up

--- src/app/page.js
+++ src/app/page.js
@@ -39,7 +39,7 @@ export default function Home() {
     >
       <main className={styles.main}>
         <h3 className={styles.text}>
-        Justmarkets Malaysia Official Site{" "}
+        Justmarkets Malaysia Official Site{"  "}
           <span
             style={{ transform: "translate(0px)", display: "inline-block" }}
           >
